6686601bd17c34392d01bab4f68245359f5bc9cd
Dorsa Moradi <dorsamoradi2016@gmail.com>
2024-10-08 11:47:49 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Library_project
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello to grupp 7!");
+        }
+    }
+}
