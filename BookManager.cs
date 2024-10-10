34133930dc2601fa37f1aea4670d534c80e4c875
IsakRegell <Isak.regell@gmail.com>
2024-10-10 10:46:57 +0200

    flyttat SearchBook och la till ViweBook för att skriva ut lista

--- BookManager.cs
+++ BookManager.cs
@@ -2,16 +2,16 @@
 
 namespace Library_project
 {
-    public class BookManager 
+    public class BookManager
     {
-       public List<Book> books = new List<Book>();
-
-       
+        public List<Book> books = new List<Book>();
 
 
-        public void AddBook(string title, string author, int isbn )
+
+
+        public void AddBook(string title, string author, int isbn)
         {
-            books.Add(new Book(title, author, isbn ));
+            books.Add(new Book(title, author, isbn));
         }
 
         public void RemoveBook(int isbn)
@@ -35,23 +35,23 @@ namespace Library_project
                 Console.WriteLine("Book not found");
             }
         }
-        public void SearchBook()
-        {
-            Console.WriteLine("Enter search term: ");
-            string searchTerm = Console.ReadLine();
-            foreach (Book book in books)
-            {
-                if (book.Title.Contains(searchTerm) || book.Author.Contains(searchTerm))
-                {
-                    book.DisplayBookInfo();
-                }
-            
-            else
-            {
-                Console.WriteLine("No books found");
-            }
-        }
-    }
+        //public void SearchBook()
+        //{
+        //    Console.WriteLine("Enter search term: ");
+        //    string searchTerm = Console.ReadLine();
+        //    foreach (Book book in books)
+        //    {
+        //        if (book.Title.Contains(searchTerm) || book.Author.Contains(searchTerm))
+        //        {
+        //            book.DisplayBookInfo();
+        //        }
+
+        //        else
+        //        {
+        //            Console.WriteLine("No books found");
+        //        }
+        //    }
+        //}
 
 
     }
