34f756393c37444f88a6b22ef0d0ccea6a85df82
Dorsa Moradi <dorsamoradi2016@gmail.com>
2024-10-10 09:25:03 +0200

    Implettering av huvudklass och dess metod

---
+++ Book.cs
@@ -0,0 +1,28 @@
+﻿namespace Library_project
+{
+    public class Book
+    {       //Attributer för huvudklassen
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public int ISBN { get; set; }
+        public bool IsCheckedOut { get; set; }
+
+        public Book(string title, string author, int isbn)
+        {
+            Title = title;
+            Author = author;
+            ISBN = isbn;
+            IsCheckedOut = false;
+        }
+        ///Metod för att skriva ut information om boken
+        public void DisplayBookInfo()
+        {
+            Console.WriteLine($"Författare: {Author}");
+            Console.WriteLine($"Titel: {Title}");
+            Console.WriteLine($"ISBN: {ISBN}");
+            Console.WriteLine($"Tillgänglighet: {(IsCheckedOut ? "Not available" : "Available")}");
+
+        }
+    }
+
+}
