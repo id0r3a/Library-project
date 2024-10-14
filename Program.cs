6cfe7b042680d62da9e1c7264c653919a9aced41
IkranMohamed <ikranmohamedliban@gmail.com>
2024-10-14 10:47:13 +0200

    klar

--- Program.cs
+++ Program.cs
@@ -5,6 +5,16 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello to grupp 7!");
+
+
+            while (true)
+            {
+                User.MenyOption();
+                
+                string chooseMenuOptions = Console.ReadLine()!;
+            }
+        }
+
+       
         }
     }
-}
--- User.cs
+++ User.cs
@@ -1,9 +1,47 @@
-﻿namespace Library_project
+﻿using System.ComponentModel.Design;
+using static System.Reflection.Metadata.BlobBuilder;
+
+namespace Library_project
 {
     public class User
     {
-          
+        public static void MenyOption()
+        {
+            Console.WriteLine("Välkomen till vårat Bibliotek");
+            Console.WriteLine("Välj ett alternativ:");
+            Console.WriteLine("1. Lägg till ny bok.");
+            Console.WriteLine("2.Sök bok efter förafattare.");
+            Console.WriteLine("3.Visa alla böcker i samlingen.");
+            Console.WriteLine("4.Checka ut/Retunera bok.");
+            Console.WriteLine("5 Stänga av programmet.");
 
+            string chooseMenuOption = Console.ReadLine();
+
+            switch (chooseMenuOption)
+
+            {
+                case "1":
+                    Console.WriteLine();
+                    break;
+
+                case "2":
+                    Console.WriteLine();
+                    break;
+
+                case "3":
+                    Console.WriteLine();
+                    break;
+
+                case "4":
+                    Console.WriteLine();
+                    break;
+                default:
+                    Console.WriteLine("Felaktigt val,försök igen.");
+                    break;
+            }
+
+        }
     }
-
 }
+
+
